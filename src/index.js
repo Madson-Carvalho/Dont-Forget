e42869b8b800bb94eb6a0be7682fa977f214118c
Madson-Carvalho <madsonraynnon@gmail.com>
2023-11-18 14:01:14 -0300

    Criado o projeto dont forget

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: "",
+  },
+  {
+    path: "",
+    element: "",
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
